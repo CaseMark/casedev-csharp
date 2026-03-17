85948621c7996612d0c2842ab68144fb2b56fca0
Dante Danelian <dante.danelian@casemark.com>
2026-03-17 17:30:41 +0000

    fix: restore stable generated tests and upload payload

--- src/Casedev.Tests/Models/Memory/V1/V1DeleteAllParamsTest.cs
+++ src/Casedev.Tests/Models/Memory/V1/V1DeleteAllParamsTest.cs
@@ -1,10 +1,28 @@
 using System;
+using System.Linq;
 using Casedev.Models.Memory.V1;
 
 namespace Casedev.Tests.Models.Memory.V1;
 
 public class V1DeleteAllParamsTest : TestBase
 {
+    static readonly char[] QuerySeparators = new[] { '&' };
+    static readonly string[] ExpectedQueryEntries = new[]
+    {
+        "tag_1=tag_1",
+        "tag_10=tag_10",
+        "tag_11=tag_11",
+        "tag_12=tag_12",
+        "tag_2=tag_2",
+        "tag_3=tag_3",
+        "tag_4=tag_4",
+        "tag_5=tag_5",
+        "tag_6=tag_6",
+        "tag_7=tag_7",
+        "tag_8=tag_8",
+        "tag_9=tag_9",
+    };
+
     [Fact]
     public void FieldRoundtrip_Works()
     {
@@ -149,12 +167,15 @@ public class V1DeleteAllParamsTest : TestBase
 
         var url = parameters.Url(new() { ApiKey = "My API Key" });
 
-        Assert.Equal(
-            new Uri(
-                "https://api.case.dev/memory/v1?tag_1=tag_1&tag_10=tag_10&tag_11=tag_11&tag_12=tag_12&tag_2=tag_2&tag_3=tag_3&tag_4=tag_4&tag_5=tag_5&tag_6=tag_6&tag_7=tag_7&tag_8=tag_8&tag_9=tag_9"
-            ),
-            url
-        );
+        var expectedQuery = ExpectedQueryEntries.OrderBy(x => x);
+
+        var actualQuery = url
+            .Query.TrimStart('?')
+            .Split(QuerySeparators, StringSplitOptions.RemoveEmptyEntries)
+            .OrderBy(x => x);
+
+        Assert.Equal("https://api.case.dev/memory/v1", url.GetLeftPart(UriPartial.Path));
+        Assert.Equal(expectedQuery, actualQuery);
     }
 
     [Fact]
